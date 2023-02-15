50e41067d772335f44bef7dd12dda5777334c56d
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2023-02-15 15:48:28 -0800

    Update personal quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,13 @@ var questionObjectArray = [
     },
     {
         question: "How would you declare an array named exArray and fill it 3 different choices in Javascript style ?",
-        choices: ["array exArray(3);","var[] exArray = {1,2,3};","var[3] exArray = []","var exArray = [1,2,3]"],
-        answer: "var exArray = [1,2,3]"
+        choices: ["array exArray(3);","var[] exArray = {1,2,3};","var[3] exArray = [];","var exArray = [1,2,3];"],
+        answer: "var exArray = [1,2,3];"
     },  
     {
-        question: "Which is the correct way to write an IF statement ?",
-        choices: ["if (i = 0){code};","if(i == 0){code};","if(i == 0)code;","if i = 0{code};"],
-        answer: "if(i == 0){code};"
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language","Home Tool Markup Language","Hyperlinks and Text Markup Language","none of the above"],
+        answer: "Hyper Text Markup Language"
     },
     {
         question: "Loops can contain these features ...",
@@ -24,7 +24,32 @@ var questionObjectArray = [
         question: "Which was created by Facebook?",
         choices: ["MongoDB Atlas","SQL","React","OPP"],
         answer: "React"
-    } 
+    },
+    {
+        question: "Choose the correct HTML element for the largest heading:",
+        choices: ["<head>","<h6>","h1","heading>"],
+        answer: "<h1>"
+    },
+    {
+        question: "Which of these elements are all <table> elements?",
+        choices: ["<table><tr><td>","<table><head><tfoot>","<table><tr><tt>","none of the above"],
+        answer: "<table><tr><td>"
+    },
+    {
+        question: "What is the correct HTML element for playing video files?",
+        choices:["<media>","<video>","<movie>","none of the ablove"],
+        answer: "<video>"
+    },
+    {
+        question: "What is the correct HTML element for playing audio files?",
+        choices: ["<audio>","<sound>","<mp3>","<auto-sound>"],
+        answer: "<audio>"
+    },
+    {
+        question: "The HTML global attribute, contenteditable is used to:",
+        choices: ["Specify whether the content of an element should be editable or not","Return the position of the first found occurrence of content inside a string","Specifies a context menu for an element. The menu appears when a user right-clicks on the element","Update content from the server"],
+        answer: "Specify whether the content of an element should be editable or not"
+    }
 ];
 
 //Created variables to hold the element needed
@@ -32,6 +57,7 @@ var currentTimeEl = document.querySelector("#currentTime");
 var timerEl = document.querySelector("#start");
 var questionsEl = document.querySelector("#questions");
 var wrapperEl = document.querySelector(".wrapper");
+
 var quizScore = 0;
 var questionIndex = 0;
 var secondsLeft = 100;
@@ -39,27 +65,26 @@ var holdInterval = 0;
 var penalty = 10;
 var ulCreate = document.createElement("ul")
 
-//click action funtion
+//click action funtion for timer to start
 timerEl.addEventListener("click", function(){
-    
     if(holdInterval === 0){
         holdInterval = setInterval(function(){
-            secondsLeft--;
-            currentTimeEl.textContent = "Time: " + secondsLeft;
+        secondsLeft--;
+        currentTimeEl.textContent = "Time: " + secondsLeft;
             
-            if(secondsLeft <= 0){
-                clearInterval(holdInterval);
-                finished();
-                currentTimeEl.textContent = "Time is up!";
-            }
-        }, 1000);
+        if(secondsLeft <= 0){
+            clearInterval(holdInterval);
+            finished();
+            currentTimeEl.textContent = "Time is up!";
+        }
+        // Interval before timer appears.
+      }, 1000);
     }
     createQuestion(questionIndex);
 });
 
 //parameter created for the questions
 function createQuestion(questionIndex){
-
     questionsEl.innerHTML = "";
     ulCreate.innerHTML = "";
 
@@ -86,16 +111,13 @@ function compareUsersChoice(event){
   
     var element = event.target;
     if(element.matches("li")){
-        
         var createdDivEl = document.createElement("div");
         createdDivEl.setAttribute("id", "createDivEl");
 
         if(element.textContent == questionObjectArray[questionIndex].answer){
             quizScore++;
             createdDivEl.textContent = "Correct! The answer is: " + questionObjectArray[questionIndex].answer;
-        }
-       
-        else{
+        }else{
             secondsLeft -= penalty;
             createdDivEl.textContent = "Wrong! The correct answer is " + questionObjectArray[questionIndex].answer  
         }
@@ -105,8 +127,7 @@ function compareUsersChoice(event){
     if(questionIndex >= questionObjectArray.length){
         finished();
         createdDivEl.textContent = "End of quiz! You got " + quizScore + "/" + questionObjectArray.length + " Correct!"; 
-    }
-    else{
+    }else{
         createQuestion(questionIndex);
     }
     questionsEl.appendChild(createdDivEl);
@@ -114,64 +135,74 @@ function compareUsersChoice(event){
 
 //function needed for finishing quiz
 function finished(){
- 
     questionsEl.innerHTML = "";
     currentTimeEl.innerHTML = "";
-    var h1El = document.createElement("h1");
-    h1El.setAttribute("id","h1El");
-    h1El.textContent = "All Done!";
-    questionsEl.appendChild(h1El);
-    var createParagraphEl = document.createElement("p");
-    createParagraphEl.setAttribute("id", "createdP");
-    questionsEl.appendChild(createParagraphEl);
 
+    // Created a heading for the finished page
+    var finishedEl = document.createElement("h1");
+    finishedEl.setAttribute("id", "finished");
+    finishedEl.textContent = "Finished!"
+
+    questionsEl.appendChild(finishedEl);
+
+    // paragraph for final score 
+    var paragraphEl = document.createElement("p");
+    paragraphEl.setAttribute("id", "paragraphEl");
+
+    questionsEl.appendChild(paragraphEl);
+
+    // Calculating score
     if(secondsLeft >= 0){
         var timeRemaining = secondsLeft;
-        var createParagraphEl2 = document.createElement("p");
         clearInterval(holdInterval);
-        createParagraphEl.textContent = "Your final score is: " + timeRemaining;
-        questionsEl.appendChild(createParagraphEl2);
+        paragraphEl.textContent = "Your final score is: " + (quizScore + timeRemaining);
+    }else{
+        clearInterval(holdInterval);
+        paragraphEl.textContent = "Your final score is: " + quizScore;
     }
 
-    var createLabelEl = document.createElement("label");
-    createLabelEl.setAttribute("type", "text");
-    createLabelEl.textContent = "Enter your initials: ";
-    questionsEl.appendChild(createLabelEl);
-    var createInputEl = document.createElement("input")
-    createInputEl.setAttribute("type", "text");
-    createInputEl.setAttribute("id", "initials");
-    createInputEl.textContent = "";
-    questionsEl.appendChild(createInputEl);
-    var createSubmitEl = document.createElement("button");
-    createSubmitEl.setAttribute("type", "submit");
-    createSubmitEl.setAttribute("id", "Submit");
-    createSubmitEl.textContent = "Submit";
-    questionsEl.appendChild(createSubmitEl);
+    // label element for initials
+    var labelEl = document.createElement("label");
+    labelEl.setAttribute("id", "labelEl");
+    labelEl.textContent = "Enter your initials: ";
 
-    createSubmitEl.addEventListener("click", function(){
-        
-        var initials = createInputEl.value;
+    questionsEl.appendChild(labelEl);
 
-        if (initials === null){
-           console.log("No initials entered")
-        }
-        else{
-            var finalScore = {
-                initials: initials,
-                score: timeRemaining
-            }
-            var allScoresKey = localStorage.getItem("allScoresKey");
-            
-            if(allScoresKey === null){
-                allScoresKey = [];
-            }
-            else{
-                allScoresKey = JSON.parse(allScoresKey);
-            }
-            allScoresKey.push(finalScore);
-            var newestScore = JSON.stringify(allScoresKey);
-            localStorage.setItem("allScoresKey", newestScore);
-            window.location.replace("./highScorePage.html");
+    // input element for initials
+    var inputEl = document.createElement("input");
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("id", "initials");
+    inputEl.setAttribute("name", "initials");
+    inputEl.textContent = "";
+
+    questionsEl.appendChild(inputEl);
+
+    // submit button
+    var submitEl = document.createElement("button");
+    submitEl.setAttribute("type", "submit");
+    submitEl.setAttribute("id", "submit");
+    submitEl.textContent = "Submit";
+
+    questionsEl.appendChild(submitEl);
+
+    // Adding event listener to the submit button
+    submitEl.addEventListener("click", function () {
+        var initials = inputEl.value;
+        if (initials === "") {
+            alert("Please enter your initials.");
+        } else {
+            var highscores =
+                JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+            var newScore = {
+                score: quizScore,
+                initials: initials
+            };
+
+            highscores.push(newScore);
+            window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+            window.location.href = "highscores.html";
         }
     });
 }
